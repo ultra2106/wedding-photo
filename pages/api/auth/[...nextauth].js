ee1ce215c7c0584f772541f331c8babde694c56a
ultra2106 <ta75005r@gmail.com>
2026-05-07 10:40:24 +0900

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,1 +1,44 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          // 主催者のドライブにアクセスするための権限
+          scope: [
+            'openid',
+            'email',
+            'profile',
+            'https://www.googleapis.com/auth/drive.file',
+            'https://www.googleapis.com/auth/spreadsheets',
+          ].join(' '),
+          access_type: 'offline',  // リフレッシュトークンを取得
+          prompt: 'consent',       // 毎回同意画面を表示（リフレッシュトークン取得のため）
+        }
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    // ログイン時にアクセストークンをセッションに保存
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.refreshToken = token.refreshToken
+      return session
+    }
+  },
+  pages: {
+    signIn: '/',  // ログインページはトップページ
+  }
+})
